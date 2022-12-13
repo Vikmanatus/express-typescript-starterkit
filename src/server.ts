dd2a7f3853ef6e903daa8bcfd99355cdddc63fd6
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-13 12:47:05 +0100

    tiny fix

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import https from 'https';
 import fs from 'fs';
 import expressApp from './app';
-import { PORT } from './config';
+import { API_URL, PORT } from './config';
 
 const options = {
   key: fs.readFileSync('./certs/dev-key.pem'),
@@ -14,4 +14,5 @@ const server = https.createServer(options, expressApp);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  console.log('Server available on this URI: ', API_URL);
 });
