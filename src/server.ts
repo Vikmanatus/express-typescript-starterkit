bf26808517e43d0f895df732fa8c45df5738f90a
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-14 16:47:10 +0100

    tiny config fix

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import https from 'https';
 import fs from 'fs';
 import expressApp from './app';
-import { API_URL, PORT } from './config';
+import { API_URL, NODE_ENV, PORT } from './config';
 
 const options = {
   key: fs.readFileSync('./certs/dev-key.pem'),
   cert: fs.readFileSync('./certs/cert.pem'),
 };
+
+/**
+ * The dev SSL certificate will not be required in production
+ * As the app will be deployed to Google Cloud App Engine, the SSL certificate will be managed automatically in production
+ */
+const devOptions = NODE_ENV === "development" ? options : {};
 /**
  * Creating HTTP server
  */
-const server = https.createServer(options, expressApp);
+const server = https.createServer(devOptions, expressApp);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
