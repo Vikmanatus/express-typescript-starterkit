211f7febf91e50427fc2ec80ceb79dc3f03c60a7
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-15 19:34:24 +0100

    temporary fix

--- src/server.ts
+++ src/server.ts
@@ -3,26 +3,26 @@ import fs from 'fs';
 import expressApp from './app';
 import { NODE_ENV, PORT } from './config';
 
-const options = {
-  key: fs.readFileSync('./certs/dev-key.pem'),
-  cert: fs.readFileSync('./certs/cert.pem'),
-};
-
 /**
  * The dev SSL certificate will not be required in production
  * As the app will be deployed to Google Cloud App Engine, the SSL certificate will be managed automatically in production
  */
-const devOptions = NODE_ENV === "development" ? options : {};
+const devOptions =
+  NODE_ENV === 'development'
+    ? {
+        key: fs.readFileSync('./certs/dev-key.pem'),
+        cert: fs.readFileSync('./certs/cert.pem'),
+      }
+    : {};
 /**
  * Creating HTTP server
  */
 const server = https.createServer(devOptions, expressApp);
 
-
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-process.on('exit', function(code) {
+process.on('exit', function (code) {
   console.log('About to exit with code:', code);
 });
