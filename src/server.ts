7c208862f06f9e74f047461b984f08d9c9863c15
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-15 20:36:46 +0100

    fix: nodejs appengine must be set to http module

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,13 @@
-//  import http from 'http';
+import http from 'http';
 import expressApp from './app';
 import { PORT } from './config';
-import https from 'https';
 
-/**
- * The dev SSL certificate will not be required in production
- * As the app will be deployed to Google Cloud App Engine, the SSL certificate will be managed automatically in production
- */
-// const devOptions =
-//   NODE_ENV === 'development'
-//     ? {
-//         key: fs.readFileSync('./certs/dev-key.pem'),
-//         cert: fs.readFileSync('./certs/cert.pem'),
-//       }
-//     : {};
 /**
  * Creating HTTP server
  */
-// const server = https.createServer(devOptions, expressApp);
-
-const server = https.createServer(expressApp)
+const server = http.createServer(expressApp);
 
 server.listen(PORT, () => {
-  console.log("Debugging port: ", process.env.PORT);
+  console.log('Debugging port: ', process.env.PORT);
   console.log(`Server listening on port ${PORT}`);
 });
-
