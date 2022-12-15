41908524da51c6d819224f1a4902d52b87b345a1
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-15 17:03:35 +0100

    WIP: trying to fix deployed app

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,35 @@
 import express, { Request } from 'express';
 import morgan from 'morgan';
-import { permissionConfig } from './config';
+import { NODE_ENV, permissionConfig } from './config';
 import { authApiLimiter } from './config/security';
 import { authRouter } from './routes';
 import { BasicJsonResponse, TypedResponse } from './types';
 import { ROUTER_ENDPOINTS } from './types/postman';
-import helmet from "helmet";
+import helmet from 'helmet';
 /**
  * Global express application
  */
 const app = express();
 
-app.use(helmet({xssFilter:true,hidePoweredBy:true}));
+app.use(helmet({ xssFilter: true, hidePoweredBy: true }));
 
-/**
- * Used to display information about incoming HTTP requests in the terminal
- */
-app.use(morgan('dev'));
+if (NODE_ENV === 'development') {
+  /**
+   * Used to display information about incoming HTTP requests in the terminal
+   */
+  app.use(morgan('dev'));
+}
 
 /**
  * Authorizing parsing of JSON body and URL encoded requests
  */
-app.use(express.json({ limit: '300kb'}));
+app.use(express.json({ limit: '300kb' }));
 app.use(express.urlencoded({ extended: true }));
 
 /**
  * Adding the rate limiter to all routes defined under /api/auth
  */
 app.use(ROUTER_ENDPOINTS.AUTH, authApiLimiter);
-
 
 /**
  * The different other endpoints used in our API
