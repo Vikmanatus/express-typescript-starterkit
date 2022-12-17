5d016a6db1480ecd7315a56539ce8432619996f7
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-12-17 06:43:57 +0100

    test sonarqube

--- src/utils/postman/index.ts
+++ src/utils/postman/index.ts
@@ -105,7 +105,7 @@ export const generatePostmanCollection = (): void => {
 
   const postmanConfigObject: PostmanObjectConfigType[] = Object.values(postmanConfig);
 
-  // TODO: refactor to foreach loop
+  // TODO: refactor map to foreach loop
   postmanConfigObject.map((element) => {
     const postmanRequestBody: OverridePostmanItemConfig = {
       name: element.requestName,
