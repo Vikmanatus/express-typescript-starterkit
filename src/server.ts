c647ca0349f817575e22efa4461fa53cc7c259b5
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-03-12 13:17:00 +0100

    clean useless log

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ import { PORT } from './config';
 const server = http.createServer(expressApp);
 
 server.listen(PORT, () => {
-  console.log('Debugging port: ', process.env.PORT);
   console.log(`Server listening on port ${PORT}`);
 });
 
