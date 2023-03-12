1cd78dda05513460fbd8e81aef9329c5b2501fc5
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-03-12 14:27:04 +0100

    added boilerplate code for routing + rate limiter

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
+import { AUTHORIZED_ENDPOINTS, AUTHORIZED_ROLES, MATCH_ENDPOINTS, PermissionConfigType } from '@/types';
 import dotenv from 'dotenv';
 
 dotenv.config({
@@ -6,3 +7,21 @@ dotenv.config({
 
 export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 export const NODE_ENV: string = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+
+/**
+ * This object is used to configure our project
+ * We define the URL of the endpoint, the authroized roles and the match URL passed
+ */
+export const permissionConfig: PermissionConfigType = {
+  home: {
+    url: AUTHORIZED_ENDPOINTS.API_ROOT_ENDPOINT,
+    authorized_roles: [AUTHORIZED_ROLES.USER, AUTHORIZED_ROLES.ADMIN, AUTHORIZED_ROLES.SUPER_ADMIN],
+    matchUrl: MATCH_ENDPOINTS.MATCH_API_ROOT_ENDPOINT,
+  },
+  authRoot: {
+    url: AUTHORIZED_ENDPOINTS.API_ROOT_ENDPOINT,
+    authorized_roles: [AUTHORIZED_ROLES.USER, AUTHORIZED_ROLES.ADMIN, AUTHORIZED_ROLES.SUPER_ADMIN],
+    matchUrl: MATCH_ENDPOINTS.MATCH_AUTH_ROOT_ENDPOINT,
+  },
+};
